696dc3d0947445a1756c2d44ce3fff2010473cd5
“[geraldgartner]git config --global user.email “[Gerald.Gartner@gmail.com] <[gerald.gartner@gmail.com]>
2016-07-06 12:50:01 +0200

    tidying

--- INEQ_wobinich.R
+++ INEQ_wobinich.R
@@ -75,3 +75,45 @@ wobinich[,'X','36-45','X','Arbeiter',2]
 # Verteilung unter allen
 alle <- data.frame(wobinich[,'X','X','X','X',2])
 
+#Load packages
+library(ggthemes)
+library(ggThemeAssist)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(git2r)
+library(formatR)
+library(scales)
+library(grid)
+library(extrafont)
+
+#Unser Style
+
+
+theme <- theme(plot.background = element_rect(fill = "gray97"), panel.grid.major = element_line(colour = "gray86", linetype = "dotted"), 
+               panel.grid.minor = element_line(colour = "gray86", linetype = "dotted")) + 
+  theme(plot.title = element_text(size = 18, face = "bold"), 
+        plot.background = element_rect(fill = "gray97", colour = "antiquewhite", size = 10, linetype = "solid")) +
+  theme(axis.ticks = element_blank(), 
+        axis.line = element_blank(),
+        axis.title = element_text(vjust = 8), 
+        panel.background = element_rect(fill = "grey97", linetype = "solid"), 
+        plot.background = element_rect(colour = "gray97"), 
+        plot.title = element_text(hjust=0, margin=unit(c(0,1,0.2,1), "cm")), 
+        plot.margin = unit(c(1,0.5,0.5,0.5), "cm")) +
+  theme(axis.text=element_text(size=14))  
+
+#df
+
+woent <- as.data.frame.table(wobinich)
+i <- sapply(woent, is.factor)
+woent[i] <- lapply(woent[i], as.character)
+
+alle <- dplyr::filter(woent, GESCH == "X", ALTER == "X", BLD == "X", SOZST == "X", TYPE=="Gew")
+
+
+
+
+
+
+
